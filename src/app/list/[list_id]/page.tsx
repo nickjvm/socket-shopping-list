c6651ce71efe6def88bccc94127f5a56211aa7bc
Nick VanMeter <van5fh@bosch.com>
2025-06-18 16:18:15 -0600

    update list page prop types

--- src/app/list/[list_id]/page.tsx
+++ src/app/list/[list_id]/page.tsx
@@ -1,10 +1,10 @@
 import ListView from "./ListView";
 import { fetchList } from "@/app/actions/lists";
-export default async function ListPage({
-  params,
-}: {
-  params: { list_id: string };
-}) {
+
+type PageProps = {
+  params: Promise<{ list_id: string }>;
+};
+export default async function ListPage({ params }: PageProps) {
   const { list_id } = await params;
   const list = await fetchList(list_id);
   return <ListView list={list} />;

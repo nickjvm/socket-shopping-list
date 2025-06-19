1f4d9917bdd9ec4d33c5259e5e13408e37d99130
Nick VanMeter <van5fh@bosch.com>
2025-06-18 18:05:06 -0600

    add logging to generate-env script

--- scripts/generate-env.js
+++ scripts/generate-env.js
@@ -3,6 +3,9 @@ import fs from "fs";
 
 const envVars = ["OPENAI_API_KEY", "TURSO_DATABASE_URL", "TURSO_AUTH_TOKEN"];
 
+console.log("Generating .env file...");
+console.log("TURSDO_DATABASE_URL:", process.env.TURSO_DATABASE_URL);
+
 const envContent = envVars
   .map((key) => `${key}=${process.env[key] || ""}`)
   .join("\n");

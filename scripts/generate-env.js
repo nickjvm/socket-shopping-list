de4247662f28f69d486cae83d213d9228a9d6b3b
Nick VanMeter <van5fh@bosch.com>
2025-06-18 20:09:24 -0600

    debug process.env keys

--- scripts/generate-env.js
+++ scripts/generate-env.js
@@ -4,7 +4,9 @@ import fs from "fs";
 const envVars = ["OPENAI_API_KEY", "TURSO_DATABASE_URL", "TURSO_AUTH_TOKEN"];
 
 console.log("Generating .env file...");
-console.log("TURSDO_DATABASE_URL:", process.env.TURSO_DATABASE_URL);
+Object.keys(process.env).forEach((key) => {
+  console.log(`${key}: ${process.env[key]}`);
+});
 
 const envContent = envVars
   .map((key) => `${key}=${process.env[key] || ""}`)
